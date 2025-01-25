2fd80f4a061d6cd9aad089e9fbc1695b869b5145
TimMeiwald <tim.meiwald@gmail.com>
2025-01-25 16:16:47 +0200

    Initial looping proc macro working, now to actually emit more than one test

--- franca-idl-parser-tests/src/test_grammar.rs
+++ franca-idl-parser-tests/src/test_grammar.rs
@@ -4,26 +4,6 @@ use std::{
     io::Read,
 };
 
-macro_rules! test_grammar_file {
-    ($path:expr) => {{
-        let path = $path;
-        let file_name = std::path::Path::new(&path).file_name().expect("Should exist");
-        
-        println!("{:?}", file_name);
-        let file_path = $path;
-        let src = fs::read_to_string(file_path);
-        let src = match src {
-            Err(err) => {
-                panic!("Error: {:?}", err)
-            }
-            Ok(src) => src,
-        };
-        shared(&src, grammar::<BasicContext>, Rules::Grammar)
-    }
-    }
-}
-
-
 use crate::shared;
 use franca_idl_parser::{grammar, BasicContext, Rules};
 #[test]
@@ -118,20 +98,6 @@ fn test_grammar_7() {
 }
 
 
-#[test]
-fn test_grammar_all_test() {
-    let file_path = "../grammar_test_files/";
-    let files = std::fs::read_dir(file_path).expect("Expected grammar_test_files dir exists");
 
-    for path in files{
-        // Actually read and test each file
-        
-        let result = test_grammar_file!(path.as_ref().expect("File should exist").path());
-        println!("{:?}", result)
-        
-    }
 
-   
-}
-
-test_grammar_files_in_dir!("../grammar_test_files/");
+test_grammar_files_in_dir!("grammar_test_files");
--- test-grammar-proc-macro/src/lib.rs
+++ test-grammar-proc-macro/src/lib.rs
@@ -2,29 +2,57 @@ extern crate proc_macro;
 extern crate quote;
 extern crate syn;
 
-use std::path::Path;
+use std::{
+    path::{self, Path},
+    str::FromStr,
+};
 
-use proc_macro2::Span;
+use proc_macro2::{Span, TokenStream};
 use syn::{ext, parse_macro_input, Ident, LitStr};
 #[proc_macro]
 pub fn test_grammar_files_in_dir(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let constant_directory = parse_macro_input!(input as LitStr).value();
-    let constant_directory = Path::new(&constant_directory);
-    let file_name = "PowerManagement.fidl";
-    let file_name = Path::new(file_name)
-        .file_stem()
-        .expect("Expected file to exist")
-        .to_str()
-        .expect("Should be able to convert to string");
-    let file_name = file_name.to_lowercase();
-    // constant_directory is a filepath for now
-    let file_name = format!("test_grammar_file_{}", file_name);
-    let test_ident = syn::Ident::new(&file_name, Span::call_site());
+    println!("{:?}", constant_directory);
+    let dir = Path::new(&constant_directory);
+    let err_msg = format!(
+        "Expected grammar_test_files dir exists: {:?}\nWarning: Path is relative to workspace root not file root for now",
+        dir
+    );
+    let files = std::fs::read_dir(dir).expect(&err_msg);
+    let mut return_stream = TokenStream::new();
+    for path in files {
+        // Actually read and test each file
+        let path = path.expect("Should exist").path();
+        let file_name = Path::new(&path)
+            .file_name()
+            .expect("Expected file to exist")
+            .to_str()
+            .expect("Should be able to convert to string");
+        let file_name_for_ident = Path::new(&path)
+            .file_stem()
+            .expect("Expected file to exist")
+            .to_str()
+            .expect("Should be able to convert to string");
+        let file_name_for_ident = file_name_for_ident.to_lowercase().replace('-', "_");
+        // constant_directory is a filepath for now
+        let file_name_for_ident = format!("test_grammar_file_{}", file_name_for_ident);
+        let test_ident = syn::Ident::new(&file_name_for_ident, Span::call_site());
+        let cwd = std::env::current_dir().unwrap();
+        let filepath = path::Path::new(&cwd).join(&constant_directory).join(file_name);
+        let filepath = filepath
+            .to_str()
+            .expect("Should be able to turn into str");
+        println!("{:?}", filepath);       
 
-    quote::quote!(
+
+
+        let filepath = syn::LitStr::new(filepath, Span::call_site());
+        println!("{:?}", constant_directory);
+        println!("{:?}", filepath);
+        return_stream = quote::quote!(
             #[test]
             fn #test_ident(){
-                let file_path = "../grammar_test_files/PowerManagement.fidl";
+                let file_path = #filepath;
                 let src = fs::read_to_string(file_path);
                 let src = match src {
                     Err(err) => {
@@ -33,9 +61,13 @@ pub fn test_grammar_files_in_dir(input: proc_macro::TokenStream) -> proc_macro::
                     Ok(src) => src,
                 };
                 let result = shared(&src, grammar::<BasicContext>, Rules::Grammar);
-                assert_eq!(result, (true, src.len() as u32));
-    })
-    .into()
+                assert_eq!(result, (true, src.len() as u32))
+        });
+        println!("{}", return_stream);
+        return return_stream.into();
+    }
+    
+    return_stream.into()
 }
 
 // let traits: Vec<&str> = vec!["std::default::Default", "std::fmt::Debug"];
